d74d16f667932f3be952673fa0b7abb071a6de8a
leandrocavaccini <leandrocavaccini2613@gmail.com>
2026-04-28 21:45:58 -0300

    1CCPY-Python_Fiap_2026

--- aula04/exercicios_aula04.py
+++ aula04/exercicios_aula04.py
@@ -96,3 +96,14 @@ for i in range(1, numero + 1):
 print(f"A soma de 1 até {numero} é: {soma}")
 
 #EXERCÍCIO 08
+
+print("---Exibindo Divisores---")
+
+num = int(input("Até qual número você deseja exibir os divisores? "))
+
+for divisores in range(1, num + 1):
+    if num % divisores == 0:
+        print(divisores)
+
+#EXERCÍCIO 09
+
