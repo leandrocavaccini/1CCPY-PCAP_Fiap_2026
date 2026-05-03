9e6bfbf6d7cb16b68df026915e17a453ed43a4b5
leandrocavaccini <leandrocavaccini2613@gmail.com>
2026-05-03 00:42:24 -0300

    1CCPY-Python_Fiap_2026

--- aula04/teste.py
+++
@@ -1,7 +0,0 @@
-print("---Exibindo Divisores---")
-
-num = int(input("Até qual número você deseja exibir os divisores? "))
-
-for divisores in range(1, num + 1):
-    if num % divisores == 0:
-        print(divisores)
