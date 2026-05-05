2bd41525dd0385f0259dacb8b7bf11463242bade
leandrocavaccini <leandrocavaccini2613@gmail.com>
2026-05-04 23:32:14 -0300

    1CCPY-Python_Fiap_2026

--- aula03/exercicios_aula03.py
+++ aula03/exercicios_aula03.py
@@ -138,3 +138,134 @@ print(f"Aumento após reajuste: {aumento:.2f}")
 print(f"Salário depois do reajuste: {salario_depois:.2f}")
 
 #EXERCÍCIO 09
+
+print("---Transporte Rodoviário---")
+
+codigo_estado_origem = int(input("Digite o código do estado de Origem: "))
+
+while codigo_estado_origem < 1 or codigo_estado_origem > 5:
+    print("Código inválido, digite um número de 1 até 5")
+    codigo_estado_origem = int(input("Digite o código do estado de Origem: "))
+
+peso_caminhao = float(input("Digite o peso do caminhão em toneladas: "))
+
+codigo_carga = int(input("Digite o código da carga: "))
+
+while codigo_carga < 10 or codigo_carga > 40:
+    print("Código inválido, digite um número de 10 até 40")
+    codigo_carga = int(input("Digite o código da carga: "))
+
+conversao_peso = peso_caminhao * 1000
+
+def preco_final_sem_imposto():
+    if codigo_carga >= 10 or codigo_carga <= 20:
+        preco_final_sem_imposto = conversao_peso * 100
+
+    elif codigo_carga >= 21 or codigo_carga <= 30:
+        preco_final_sem_imposto = conversao_peso * 250
+
+    elif codigo_carga >= 31 or codigo_carga <= 40:
+        preco_final_sem_imposto = conversao_peso * 340
+
+    return preco_final_sem_imposto
+
+match codigo_estado_origem:
+    case 1:
+        imposto = 35/100
+    case 2:
+        imposto = 25/100
+    case 3:
+        imposto = 15/100
+    case 4:
+        imposto = 5/100
+    case 5:
+        imposto = 1
+
+preco_imposto = preco_final_sem_imposto() * imposto
+preco_final_com_imposto = preco_final_sem_imposto() + (preco_final_sem_imposto() * imposto)
+
+print("---INFORMAÇÕES FINAIS---")
+print(f"O peso do caminhão em quilos (kg) é de: {conversao_peso}kg")
+print(f"O preço da carga sem imposto é de: R${preco_final_sem_imposto()}")
+print(f"O valor do imposto é de {imposto * 100}% que do preço da carga totaliza: R${preco_imposto}")
+print(f"O valor final da carga é de: R${preco_final_com_imposto} ")
+
+#EXERCÍCIO 10
+
+print("---Classificando Triângulos---")
+
+ladoA = float(input("Lado A do triângulo: "))
+ladoB = float(input("Lado B do triângulo: "))
+ladoC = float(input("Lado C do triângulo: "))
+
+if ladoA == ladoB == ladoC:
+    ordem_decrescente = ladoA, ladoB, ladoC
+
+elif ladoA > ladoB == ladoC:
+    ordem_decrescente = ladoA, ladoB, ladoC
+
+elif ladoA == ladoB > ladoC:
+    ordem_decrescente = ladoA, ladoB,  ladoC
+
+elif ladoB > ladoA == ladoC:
+    ordem_decrescente = ladoB, ladoA, ladoC
+
+elif ladoB == ladoC > ladoA:
+    ordem_decrescente = ladoB, ladoC, ladoA
+
+elif ladoC > ladoA == ladoB:
+    ordem_decrescente = ladoC, ladoA, ladoB
+
+elif ladoC == ladoA > ladoB:
+    ordem_decrescente = ladoC, ladoA, ladoB
+
+elif ladoA > ladoB > ladoC:
+    ordem_decrescente = ladoA, ladoB, ladoC
+
+elif ladoA > ladoC > ladoB:
+    ordem_decrescente = ladoA, ladoC, ladoB
+
+elif ladoB > ladoA > ladoC:
+    ordem_decrescente = ladoB, ladoA, ladoC
+
+elif ladoB > ladoC > ladoA:
+    ordem_decrescente = ladoB, ladoC, ladoA
+
+elif ladoC > ladoA > ladoB:
+    ordem_decrescente = ladoC, ladoA, ladoB
+
+elif ladoC > ladoB > ladoA:
+    ordem_decrescente = ladoC, ladoB, ladoA
+
+if ladoA >= (ladoB + ladoC):
+    print(f"Não forma Triângulo")
+
+if ladoA ** 2 == ((ladoB ** 2) + ( ladoC ** 2)):
+    print(f"É um Triângulo Retângulo")
+
+if ladoA ** 2 > ((ladoB ** 2) + ( ladoC ** 2)):
+    print(f"É um Triângulo Obtusângulo")
+
+if ladoA ** 2 < ((ladoB ** 2) + (ladoC ** 2)):
+    print(f"É um Triângulo Acutângulo")
+
+if ladoA == ladoB == ladoC:
+    print(f"É um Triângulo Equilátero")
+
+if ladoA > ladoB == ladoC:
+    print(f"É um Triângulo Isóceles")
+
+if ladoA == ladoB > ladoC:
+    print(f"É um Triângulo Isóceles")
+
+if ladoB > ladoA == ladoC:
+    print(f"É um Triângulo Isóceles")
+
+if ladoB == ladoC > ladoA:
+    print(f"É um Triângulo Isóceles")
+
+if ladoC > ladoA == ladoB:
+    print(f"É um Triângulo Isóceles")
+
+if ladoC == ladoA > ladoB:
+    print(f"É um Triângulo Isóceles")
