6c568749891e59a991f2bbdc8ca28f70406b82ec
leandrocavaccini <leandrocavaccini2613@gmail.com>
2026-07-11 00:31:53 -0300

    1CCPY-PCAP_Fiap_2026

--- aula05/exercicios_aula05/exercicio07_aula05.py
+++ aula05/exercicios_aula05/exercicio07_aula05.py
@@ -1,4 +1,23 @@
 #EXERCÍCIO 07
 
+print("\n---Criando uma Matriz 3x4---\n")
+
 import random
 
+matriz = []
+
+for i in range(0, 3):
+    matriz_linhas = []
+    matriz.append(matriz_linhas)
+    for j in range(0, 4):
+        matriz_linhas.append(random.randint(1, 10))
+
+print("Matriz linear:\n")
+print(matriz)
+
+print()
+
+print("Matriz montada:\n")
+print(matriz[0])
+print(matriz[1])
+print(matriz[2])
